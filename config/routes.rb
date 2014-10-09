cac62f4e85f2f8842a44b836e7d78bf2cb0fa366
alfonsopintos <alfonsopintos@gmail.com>
2014-10-09 17:50:03 -0400

    about, portfolio, skills, contact: ready

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'dashboard/skills'
   get 'dashboard/contact'
 
-  root 'dashboard#index'
+  root 'dashboard#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
